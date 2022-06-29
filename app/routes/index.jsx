b9361c074d38f321c5dcb10f77f0e5cc5519b981
JB Hutch <jbhutch01@gmail.com>
2022-06-29 00:26:31 -0400

    Reordering sort of working

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,27 +1,68 @@
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import { json } from '@remix-run/node'
 import { Form } from '@remix-run/react'
 import { Button, TextInput } from 'flowbite-react'
 import { GripVertical, Trash2 } from 'lucide-react'
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 import { useGetNanoId } from '../hooks/useGetNanoId'
 
 export default function Index() {
-  const [options, setOptions] = useState(['a', 'b', 'c'])
+  const [activeId, setActiveId] = useState(null)
+  const [items, setItems] = useState(['a', 'b', 'c'])
 
-  const addOption = () => {
-    setOptions([...options, ''])
+  const addItem = () => {
+    setItems([...items, ''])
   }
 
-  const updateOption = (index, value) => {
-    const newOptions = [...options]
-    newOptions[index] = value
-    setOptions(newOptions)
+  const updateItem = (index, value) => {
+    const newItems = [...items]
+    newItems[index] = value
+    setItems(newItems)
   }
 
-  const removeOption = (index) => {
-    const newOptions = [...options]
-    newOptions.splice(index, 1)
-    setOptions(newOptions)
+  const removeItem = (index) => {
+    const newItems = [...items]
+    newItems.splice(index, 1)
+    setItems(newItems)
+  }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+
+  const handleDragEnd = (event) => {
+    console.log(event)
+
+    const { active, over } = event
+
+    if (active && over && active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id)
+        const newIndex = items.indexOf(over.id)
+        const test = arrayMove(items, oldIndex, newIndex)
+        console.log({ test })
+        return test
+      })
+    }
   }
 
   return (
@@ -37,32 +78,32 @@ export default function Index() {
         </div>
         <Form method="post">
           <div className="mb-8 rounded-md border border-slate-300 bg-slate-200 p-4">
-            {options.map((option, idx) => (
-              <div key={idx} className="mb-4 rounded-md bg-white p-4 shadow-sm">
-                <div className="flex items-center">
-                  <div className="pr-4 text-slate-500">
-                    <GripVertical />
-                  </div>
-                  <div className="flex-1">
-                    <TextInput
-                      name="option"
-                      value={option}
-                      onChange={(e) => updateOption(idx, e.target.value)}
-                    />
-                  </div>
-                  {options.length > 1 ? (
-                    <div className="pl-4 text-slate-500">
-                      <Button onClick={() => removeOption(idx)} color="light">
-                        <Trash2 className="h-4 w-4 text-red-500" />
-                      </Button>
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-            ))}
+            <DndContext
+              sensors={sensors}
+              onDragEnd={handleDragEnd}
+              onDragStart={({ active }) => setActiveId(active.id)}
+            >
+              <SortableContext
+                items={items}
+                strategy={verticalListSortingStrategy}
+                collisionDetection={closestCenter}
+              >
+                {items.map((item, idx) => (
+                  <SortableItem
+                    key={idx}
+                    idx={item}
+                    value={item}
+                    onChange={(e) => updateItem(idx, e.target.value)}
+                    removeItem={() => removeItem(idx)}
+                    isShowingDelete={items.length > 1}
+                  />
+                ))}
+              </SortableContext>
+              <DragOverlay>{activeId ? <Item /> : null}</DragOverlay>
+            </DndContext>
             <div className="flex justify-center">
-              <Button color="light" onClick={addOption}>
-                Add an Option
+              <Button color="light" onClick={addItem}>
+                Add an Item
               </Button>
             </div>
           </div>
@@ -75,9 +116,68 @@ export default function Index() {
   )
 }
 
+const SortableItem = ({
+  idx,
+  value,
+  onChange,
+  removeItem,
+  isShowingDelete,
+}) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: idx })
+
+  console.log({ transform, transition })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
+  return (
+    <Item
+      style={style}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      value={value}
+      onChange={onChange}
+      removeItem={removeItem}
+      isShowingDelete={isShowingDelete}
+    />
+  )
+}
+
+const Item = forwardRef(
+  ({ onChange, value, isShowingDelete, removeItem, ...props }, ref) => {
+    return (
+      <div
+        {...props}
+        ref={ref}
+        className="mb-4 rounded-md bg-white p-4 shadow-sm"
+      >
+        <div className="flex items-center">
+          <div className="pr-4 text-slate-500">
+            <GripVertical />
+          </div>
+          <div className="flex-1">
+            <TextInput name="item" value={value} onChange={onChange} />
+          </div>
+          {isShowingDelete > 1 ? (
+            <div className="pl-4 text-slate-500">
+              <Button onClick={removeItem} color="light">
+                <Trash2 className="h-4 w-4 text-red-500" />
+              </Button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    )
+  }
+)
+
 export async function action({ request }) {
   const formData = await request.formData()
-  const options = formData.getAll('option')
+  const items = formData.getAll('item')
   const uniqeSlug = useGetNanoId()
   return json({ uniqeSlug })
 }
