9fb937abf51643544ff5d81464876f292fac58f3
JB Hutch <jbhutch01@gmail.com>
2022-06-29 07:54:34 -0400

    Use react-beautiful-dnd instead

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,27 +1,57 @@
 import { json } from '@remix-run/node'
 import { Form } from '@remix-run/react'
+import clsx from 'clsx'
 import { Button, TextInput } from 'flowbite-react'
 import { GripVertical, Trash2 } from 'lucide-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { useGetNanoId } from '../hooks/useGetNanoId'
 
 export default function Index() {
-  const [options, setOptions] = useState(['a', 'b', 'c'])
+  const [items, setItems] = useState(['a', 'b', 'c'])
+  const [isWindowReady, setIsWindowReady] = useState(false)
 
-  const addOption = () => {
-    setOptions([...options, ''])
+  useEffect(() => {
+    setIsWindowReady(true)
+  }, [])
+
+  const addItem = () => {
+    setItems([...items, ''])
   }
 
-  const updateOption = (index, value) => {
-    const newOptions = [...options]
-    newOptions[index] = value
-    setOptions(newOptions)
+  const updateItem = (index, value) => {
+    const newItems = [...items]
+    newItems[index] = value
+    setItems(newItems)
   }
 
-  const removeOption = (index) => {
-    const newOptions = [...options]
-    newOptions.splice(index, 1)
-    setOptions(newOptions)
+  const removeItem = (index) => {
+    const newItems = [...items]
+    newItems.splice(index, 1)
+    setItems(newItems)
+  }
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+
+    return result
+  }
+
+  const handleDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return
+    }
+
+    const newItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    )
+
+    setItems(newItems)
   }
 
   return (
@@ -37,32 +67,66 @@ export default function Index() {
         </div>
         <Form method="post">
           <div className="mb-8 rounded-md border border-slate-300 bg-slate-200 p-4">
-            {options.map((option, idx) => (
-              <div key={idx} className="mb-4 rounded-md bg-white p-4 shadow-sm">
-                <div className="flex items-center">
-                  <div className="pr-4 text-slate-500">
-                    <GripVertical />
-                  </div>
-                  <div className="flex-1">
-                    <TextInput
-                      name="option"
-                      value={option}
-                      onChange={(e) => updateOption(idx, e.target.value)}
-                    />
-                  </div>
-                  {options.length > 1 ? (
-                    <div className="pl-4 text-slate-500">
-                      <Button onClick={() => removeOption(idx)} color="light">
-                        <Trash2 className="h-4 w-4 text-red-500" />
-                      </Button>
+            {isWindowReady ? (
+              <DragDropContext onDragEnd={handleDragEnd}>
+                <Droppable droppableId="droppable">
+                  {(provided, snapshot) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                      {items.map((item, idx) => (
+                        <Draggable key={item} draggableId={item} index={idx}>
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className="mb-4"
+                            >
+                              <div
+                                className={clsx(
+                                  'rounded-md bg-white p-4 transition-all',
+                                  snapshot.isDragging
+                                    ? 'user-select-none scale-105 shadow-2xl'
+                                    : 'scale-100 shadow-sm'
+                                )}
+                              >
+                                <div className="flex items-center">
+                                  <div className="pr-4 text-slate-500">
+                                    <GripVertical />
+                                  </div>
+                                  <div className="flex-1">
+                                    <TextInput
+                                      name="item"
+                                      value={item}
+                                      onChange={(e) =>
+                                        updateItem(idx, e.target.value)
+                                      }
+                                    />
+                                  </div>
+                                  {items.length > 1 ? (
+                                    <div className="pl-4 text-slate-500">
+                                      <Button
+                                        onClick={() => removeItem(idx)}
+                                        color="light"
+                                      >
+                                        <Trash2 className="h-4 w-4 text-red-500" />
+                                      </Button>
+                                    </div>
+                                  ) : null}
+                                </div>
+                              </div>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
                     </div>
-                  ) : null}
-                </div>
-              </div>
-            ))}
+                  )}
+                </Droppable>
+              </DragDropContext>
+            ) : null}
             <div className="flex justify-center">
-              <Button color="light" onClick={addOption}>
-                Add an Option
+              <Button color="light" onClick={addItem}>
+                Add an Item
               </Button>
             </div>
           </div>
@@ -77,7 +141,7 @@ export default function Index() {
 
 export async function action({ request }) {
   const formData = await request.formData()
-  const options = formData.getAll('option')
+  const items = formData.getAll('item')
   const uniqeSlug = useGetNanoId()
   return json({ uniqeSlug })
 }
