0ee2bc409da417b6f639f21da97de7cc5509dc3b
JB Hutch <jbhutch01@gmail.com>
2022-06-30 10:51:42 -0400

    Organize class names

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -150,19 +150,22 @@ export default function Index() {
 }
 
 const GrowingTextarea = ({ value, onChange, className, ...props }) => {
+  const sharedStyles = 'border py-2 px-3'
+  const textareaStyles =
+    'absolute top-0 left-0 h-full w-full resize-none overflow-hidden'
+  const growableStyles = 'invisible min-h-[2.625rem] whitespace-pre-wrap'
+
   return (
     <div className={clsx('relative', className)}>
-      <div
-        aria-hidden="true"
-        className="invisible min-h-[2.625rem] whitespace-pre-wrap border py-2 px-3"
-      >
+      <div aria-hidden="true" className={clsx(sharedStyles, growableStyles)}>
         {value}
       </div>
       <textarea
         {...props}
         className={clsx(
-          'absolute top-0 left-0 h-full w-full resize-none overflow-hidden py-2 px-3',
-          'focus:blue-indigo-500 block rounded-md border border-gray-300 bg-gray-50 focus:border-blue-500'
+          sharedStyles,
+          textareaStyles,
+          'focus:blue-indigo-500 block rounded-md border-gray-300 bg-gray-50 focus:border-blue-500'
         )}
         value={value}
         onChange={onChange}
