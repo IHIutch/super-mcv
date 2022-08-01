a2aa11f7b9285d3866251ea9351805f116fd67d5
JB Hutch <jbhutch01@gmail.com>
2022-08-01 13:49:13 -0400

    Add buy me a coffee link

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,25 +3,38 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="mt-64 border-t py-4 text-center">
-      Special thanks to{' '}
-      <a
-        className="font-medium text-blue-500 underline"
-        href="https://hashnode.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Hashnode
-      </a>{' '}
-      and{' '}
-      <a
-        className="font-medium text-blue-500 underline"
-        href="https://planetscale.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        PlanetScale
-      </a>
-      .
+      <div className="mb-2">
+        Special thanks to{' '}
+        <a
+          className="font-medium text-blue-500 underline"
+          href="https://hashnode.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Hashnode
+        </a>{' '}
+        and{' '}
+        <a
+          className="font-medium text-blue-500 underline"
+          href="https://planetscale.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          PlanetScale
+        </a>
+        .
+      </div>
+      <div>
+        Find this app useful?{' '}
+        <a
+          className="font-medium text-blue-500 underline"
+          href="https://www.buymeacoffee.com/ihiutch"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Buy Me a Coffee!
+        </a>
+      </div>
     </div>
   )
 }
